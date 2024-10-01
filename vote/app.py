897c293fe4950fe096430c9a67d3a251f9132950
Zubair <zubair.aslam@docker.com>
2024-10-01 11:54:00 +1000

    testworkflow

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 import logging
 
-option_a = os.getenv('OPTION_A', "CATS")
+option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
